49f10bb010c7d5da25ebefa2e995225b5667f028
lawrencemboya <lawrencemboya@gmail.com>
2019-07-09 00:12:42 -0400

    first commit

---
+++ src/main/java/wcci/Album_collection_API/AlbumCollectionApiApplication.java
@@ -0,0 +1,13 @@
+package wcci.Album_collection_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumCollectionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumCollectionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wcci/Album_collection_API/AlbumCollectionApiApplicationTests.java
@@ -0,0 +1,16 @@
+package wcci.Album_collection_API;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlbumCollectionApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
